855e67855d0b3f712764e3733471f1b930192c19
SaltyQuetzals <quontas@gmail.com>
2018-04-21 15:00:10 -0500

    WIP, can't access models right now for unknown reason.

---
+++ scrapers/scrape-city-council.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+import os
+import django
+
+ROOT_URL = 'http://dallascityhall.com/'
+URL = 'http://dallascityhall.com/government/Pages/city-council.aspx'
+
+sys.path.append(
+    os.path.realpath(
+        os.path.join(
+            os.path.dirname(__file__),
+            '../server/')))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+django.setup()
+
+from core.models import Representative
+
+def follow_url(URL):
+    """
+    Just a wrapper function that automatically raises
+    an error if there's issues requesting.
+    """
+    r = requests.get(URL)
+    r.raise_for_status()
+    return r.text
+
+
+html = follow_url(URL)
+
+soup = BeautifulSoup(html, 'html.parser')
+
+member_blocks = soup.select('#memberBlocks')
+
+
+for member_block in member_blocks:
+    link = [(ROOT_URL + a['href'])
+            for a in member_block.find_all('a', href=True)][0]
+    district_num = member_block.select_one(
+        'span').text.strip().rsplit(' ', 1)[1]
+    district_num = district_num.replace(u'\u200b', '')
+    district_num = int(district_num)
+    council_member_name = member_block.select_one('h2')
+
+    # Get representatives' contact information
+    contact_html = follow_url(link)
+    contact_soup = BeautifulSoup(contact_html, 'html.parser')
+
+    # City of Dallas, why is everything labeled as a "phone"?
+    primary_contact_email = contact_soup.select_one('.phone')
+    rep = Representative(name=council_member_name, email=primary_contact_email)
+    print(rep)
--- server/core/models.py
+++ server/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Representative(models.Model):
+    name = models.CharField(max_length=128)
+    email = models.EmailField(max_length=256, null=True)
+
+    class Meta:
+        ordering = ('name',)
